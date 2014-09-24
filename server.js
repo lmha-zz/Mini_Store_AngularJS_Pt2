dd160899235e9faaab33b0d6e2405443a4e89ef7
Lisa Ha <lisamayha@gmail.com>
2014-09-24 15:00:04 -0700

    new server.js file and bin dir

--- server.js
+++ server.js
@@ -1,23 +1,56 @@
 var express = require('express');
-var http = require('http');
 var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var config = require('./config/mongoose');
+
 var app = express();
-var config = require('./config/mongoose')
-//all environments
+var routes = require('./config/routes')(app);
+
+// view engine setup
 app.set('views', path.join(__dirname, './server/views'));
 app.set('view engine', 'ejs');
-// app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//sets environment timezone
-process.env.TZ = "PST"
-//development only
-if('development' == app.get('env')) {
-    app.use(express.errorHandler());
+
+// app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
-var routes = require('./config/routes')(app);
-app.listen(process.env.PORT || 3000);
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
