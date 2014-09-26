57539bc518d2b08bef9bbc5c223790e955d06623
Lisa Ha <lisamayha@gmail.com>
2014-09-26 11:32:24 -0700

    fixed server file, data can be sent from angular to express now

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
 	$('a').click(function() {
 		$('.active').attr('class', '');
 		$(this).parent().attr('class', 'active');
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var config = require('./config/mongoose');
 
 var app = express();
-var routes = require('./config/routes')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, './server/views'));
@@ -20,6 +19,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var routes = require('./config/routes')(app);
 // app.use('/', routes);
 
 /// catch 404 and forward to error handler
